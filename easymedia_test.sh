aaff158d467fc0df6d1f2f5ee16ca4782fa15ced
Cody Xie <cody.xie@rock-chips.com>
2019-10-25 11:08:17 +0800

    Change project license to BSD-3-Clause
    Change-Id: I48e5de8082457a2b2d7d363073b736d09e2deccd
    Signed-off-by: Cody Xie <cody.xie@rock-chips.com>

--- easymedia_test.sh
+++ easymedia_test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+##
+## Copyright 2019 Fuzhou Rockchip Electronics Co., Ltd. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the LICENSE file.
+##
 
 TOPDIR=$(cd $(dirname $(readlink -f $0));pwd)
 CHIP=$1
--- include/easymedia/muxer.h
+++ include/easymedia/muxer.h
@@ -1,23 +1,6 @@
-/*
- * Copyright (C) 2017 Hertz Wang 1989wanghang@163.com
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see http://www.gnu.org/licenses
- *
- * Any non-GPL usage of this software or parts of this software is strictly
- * forbidden.
- *
- */
+// Copyright 2019 Fuzhou Rockchip Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef EASYMEDIA_MUXER_H_
 #define EASYMEDIA_MUXER_H_
