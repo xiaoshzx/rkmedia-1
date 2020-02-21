f141c6f6a3a96db668af2efc60eaf795bd6f5dbb
Francis Fan <francis.fan@rock-chips.com>
2020-02-21 09:53:37 +0800

    flow test: add copyright information to test code
    Change-Id: I56bb8792a0580654e85ad41c8c739fd320b9e8f5
    Signed-off-by: Francis Fan <francis.fan@rock-chips.com>

--- src/flow/test/muxer_flow_test.cc
+++ src/flow/test/muxer_flow_test.cc
@@ -1,3 +1,7 @@
+// Copyright 2020 Fuzhou Rockchip Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "stdint.h"
 #include "stdio.h"
 #include "unistd.h"
--- src/flow/test/rga_filter_flow_test.cc
+++ src/flow/test/rga_filter_flow_test.cc
@@ -1,3 +1,7 @@
+// Copyright 2020 Fuzhou Rockchip Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include <assert.h>
 #include <fcntl.h>
 #include <stdio.h>
