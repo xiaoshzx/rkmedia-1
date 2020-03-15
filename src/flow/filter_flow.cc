50dfeaf4614de4b9bf1f00ce1ca073ed2a1a40a5
Yu YongZhen <yuyz@rock-chips.com>
2020-03-16 01:21:27 +0800

    filter: add ffmpeg audio fifo filter to fixed output number samples
    Signed-off-by: Yu YongZhen <yuyz@rock-chips.com>
    Change-Id: I68406e8f46cde7d07dd83cfa00fddbe5a8f87167

--- src/flow/filter_flow.cc
+++ src/flow/filter_flow.cc
@@ -98,8 +98,8 @@ FilterFlow::FilterFlow(const char *param)
       }
     }
   } else {
-    LOG_TODO();
-    SetError(-EINVAL);
+    // support async mode (one input, multi output)
+    support_async = true;
     return;
   }
 }
