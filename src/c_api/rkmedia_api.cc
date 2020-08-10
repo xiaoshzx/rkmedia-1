3bcc39aaeb45b5af7f2e50c7e017399a2524b83e
Francis Fan <francis.fan@rock-chips.com>
2020-08-10 14:08:43 +0800

    c api: fix rga bind next chn error
    Signed-off-by: Francis Fan <francis.fan@rock-chips.com>
    Change-Id: Ib4e4b70888ce13eb6ecb02f2a2ae981a6e0d1068

--- src/c_api/rkmedia_api.cc
+++ src/c_api/rkmedia_api.cc
@@ -231,6 +231,10 @@ RK_S32 RK_MPI_SYS_Bind(const MPP_CHN_S *pstSrcChn,
     src = g_aenc_chns[pstSrcChn->s32ChnId].rkmedia_flow;
     src_chn = &g_aenc_chns[pstSrcChn->s32ChnId];
     break;
+  case RK_ID_RGA:
+    src = g_rga_chns[pstDestChn->s32ChnId].rkmedia_flow;
+    src_chn = &g_rga_chns[pstDestChn->s32ChnId];
+    break;
   default:
     return -RK_ERR_SYS_NOT_SUPPORT;
   }
