5e6a3b25f19a19e41be06ed987c5376aac31c5df
Francis Fan <francis.fan@rock-chips.com>
2020-09-02 14:46:11 +0800

    c api: examples: update rkmedia_vi_od_test
    note:
    1. support display
    2. support init and deinit stress test
    
    Signed-off-by: Francis Fan <francis.fan@rock-chips.com>
    Change-Id: I9218c649858652da7748cbe6b0e25890a7dd4957

--- examples/rkmedia_vi_od_test.c
+++ examples/rkmedia_vi_od_test.c
@@ -36,25 +36,8 @@ void occlusion_detection_cb(EVENT_S *pstEvent) {
   }
 }
 
-int main() {
-  RK_S32 ret = 0;
-
-  RK_MPI_SYS_Init();
-
-  VI_CHN_ATTR_S vi_chn_attr;
-  vi_chn_attr.pcVideoNode = "rkispp_scale0";
-  vi_chn_attr.u32BufCnt = 4;
-  vi_chn_attr.u32Width = 1920;
-  vi_chn_attr.u32Height = 1080;
-  vi_chn_attr.enPixFmt = IMAGE_TYPE_NV12;
-  vi_chn_attr.enWorkMode = VI_WORK_MODE_NORMAL;
-  ret = RK_MPI_VI_SetChnAttr(0, 0, &vi_chn_attr);
-  ret |= RK_MPI_VI_EnableChn(0, 0);
-  if (ret) {
-    printf("ERROR: Vi Create failed! ret=%d\n", ret);
-    exit(0);
-  }
-
+static int enable_occlusion_detection() {
+  printf("+++++++++++ Enable OD start ++++++++++++++++++\n");
   ALGO_OD_ATTR_S stOdChnAttr;
   stOdChnAttr.enImageType = IMAGE_TYPE_NV12;
   stOdChnAttr.u32Width = 1920;
@@ -65,10 +48,10 @@ int main() {
   stOdChnAttr.stRoiRects[0].u32Width = 1920;
   stOdChnAttr.stRoiRects[0].u32Height = 1080;
   stOdChnAttr.u16Sensitivity = 30;
-  ret = RK_MPI_ALGO_OD_CreateChn(0, &stOdChnAttr);
+  int ret = RK_MPI_ALGO_OD_CreateChn(0, &stOdChnAttr);
   if (ret) {
     printf("ERROR: OcclusionDetection Create failed! ret=%d\n", ret);
-    exit(0);
+    return -1;
   }
 
   MPP_CHN_S stEncChn;
@@ -78,36 +61,168 @@ int main() {
   ret = RK_MPI_SYS_RegisterEventCb(&stEncChn, occlusion_detection_cb);
   if (ret) {
     printf("ERROR: MoveDetection register event failed! ret=%d\n", ret);
-    exit(0);
+    return -1;
   }
 
-  MPP_CHN_S stSrcChn;
-  MPP_CHN_S stDestChn;
-
+  MPP_CHN_S stSrcChn = {0};
+  MPP_CHN_S stDestChn = {0};
   stSrcChn.enModId = RK_ID_VI;
-  stSrcChn.s32DevId = 0;
   stSrcChn.s32ChnId = 0;
-
   stDestChn.enModId = RK_ID_ALGO_OD;
-  stDestChn.s32DevId = 0;
   stDestChn.s32ChnId = 0;
   ret = RK_MPI_SYS_Bind(&stSrcChn, &stDestChn);
   if (ret) {
     printf("ERROR: Bind vi and od failed! ret=%d\n", ret);
-    exit(0);
+    return -1;
+  }
+
+  printf("+++++++++++ Enable OD end ++++++++++++++++++\n");
+  return 0;
+}
+
+static int disable_occlusion_detection() {
+  printf("+++++++++++ Disable OD start ++++++++++++++++++\n");
+  MPP_CHN_S stSrcChn = {0};
+  MPP_CHN_S stDestChn = {0};
+  stSrcChn.enModId = RK_ID_VI;
+  stSrcChn.s32ChnId = 0;
+  stDestChn.enModId = RK_ID_ALGO_OD;
+  stDestChn.s32ChnId = 0;
+  int ret = RK_MPI_SYS_UnBind(&stSrcChn, &stDestChn);
+  if (ret) {
+    printf("UnBind vi[0] to od[0] failed! ret=%d\n", ret);
+    return -1;
+  }
+
+  RK_MPI_ALGO_OD_DestroyChn(0);
+  printf("+++++++++++ Disable OD end ++++++++++++++++++\n");
+  return 0;
+}
+
+int main() {
+  RK_S32 ret = 0;
+  RK_S32 video_width = 1920;
+  RK_S32 video_height = 1080;
+  RK_S32 disp_widht = 720;
+  RK_S32 disp_height = 1280;
+
+  RK_MPI_SYS_Init();
+
+  VI_CHN_ATTR_S vi_chn_attr;
+  vi_chn_attr.pcVideoNode = "rkispp_scale0";
+  vi_chn_attr.u32BufCnt = 4;
+  vi_chn_attr.u32Width = video_width;
+  vi_chn_attr.u32Height = video_height;
+  vi_chn_attr.enPixFmt = IMAGE_TYPE_NV12;
+  vi_chn_attr.enWorkMode = VI_WORK_MODE_NORMAL;
+  ret = RK_MPI_VI_SetChnAttr(0, 0, &vi_chn_attr);
+  ret |= RK_MPI_VI_EnableChn(0, 0);
+  if (ret) {
+    printf("Create vi[1] failed! ret=%d\n", ret);
+    return -1;
+  }
+
+  RGA_ATTR_S stRgaAttr;
+  stRgaAttr.bEnBufPool = RK_TRUE;
+  stRgaAttr.u16BufPoolCnt = 12;
+  stRgaAttr.u16Rotaion = 90;
+  stRgaAttr.stImgIn.u32X = 0;
+  stRgaAttr.stImgIn.u32Y = 0;
+  stRgaAttr.stImgIn.imgType = IMAGE_TYPE_NV12;
+  stRgaAttr.stImgIn.u32Width = video_width;
+  stRgaAttr.stImgIn.u32Height = video_height;
+  stRgaAttr.stImgIn.u32HorStride = video_width;
+  stRgaAttr.stImgIn.u32VirStride = video_height;
+  stRgaAttr.stImgOut.u32X = 0;
+  stRgaAttr.stImgOut.u32Y = 0;
+  stRgaAttr.stImgOut.imgType = IMAGE_TYPE_RGB888;
+  stRgaAttr.stImgOut.u32Width = disp_widht;
+  stRgaAttr.stImgOut.u32Height = disp_height;
+  stRgaAttr.stImgOut.u32HorStride = disp_widht;
+  stRgaAttr.stImgOut.u32VirStride = disp_height;
+  ret = RK_MPI_RGA_CreateChn(0, &stRgaAttr);
+  if (ret) {
+    printf("Create rga[0] falied! ret=%d\n", ret);
+    return -1;
+  }
+
+  VO_CHN_ATTR_S stVoAttr = {0};
+  stVoAttr.enImgType = IMAGE_TYPE_RGB888;
+  stVoAttr.u16Fps = 60;
+  stVoAttr.u16Zpos = 0;
+  stVoAttr.u32Width = disp_widht;
+  stVoAttr.u32Height = disp_height;
+  stVoAttr.u32HorStride = disp_widht;
+  stVoAttr.u32VerStride = disp_height;
+  ret = RK_MPI_VO_CreateChn(0, &stVoAttr);
+  if (ret) {
+    printf("Create vo[0] failed! ret=%d\n", ret);
+    return -1;
+  }
+
+  MPP_CHN_S stSrcChn = {0};
+  MPP_CHN_S stDestChn = {0};
+
+  // RGA -> VO
+  stSrcChn.enModId = RK_ID_RGA;
+  stSrcChn.s32ChnId = 0;
+  stDestChn.enModId = RK_ID_VO;
+  stDestChn.s32ChnId = 0;
+  ret = RK_MPI_SYS_Bind(&stSrcChn, &stDestChn);
+  if (ret) {
+    printf("Bind vi[0] to rga[0] failed! ret=%d\n", ret);
+    return -1;
+  }
+
+  // VI -> RGA
+  stSrcChn.enModId = RK_ID_VI;
+  stSrcChn.s32ChnId = 0;
+  stDestChn.enModId = RK_ID_RGA;
+  stDestChn.s32ChnId = 0;
+  ret = RK_MPI_SYS_Bind(&stSrcChn, &stDestChn);
+  if (ret) {
+    printf("Bind vi[0] to rga[0] failed! ret=%d\n", ret);
+    return -1;
   }
 
   printf("%s initial finish\n", __func__);
   signal(SIGINT, sigterm_handler);
 
-  while (!quit) {
-    usleep(100);
+  while(!quit) {
+    printf("Keep OD working on 10s...\n");
+    if(enable_occlusion_detection())
+      break;
+    sleep(10);
+    printf("Keep OD closing on 3s...\n");
+    if(disable_occlusion_detection())
+      break;
+    sleep(3);
+  }
+  printf("%s exit!\n", __func__);
+
+  stSrcChn.enModId = RK_ID_RGA;
+  stSrcChn.s32ChnId = 0;
+  stDestChn.enModId = RK_ID_VO;
+  stDestChn.s32ChnId = 0;
+  ret = RK_MPI_SYS_UnBind(&stSrcChn, &stDestChn);
+  if (ret) {
+    printf("UnBind rga[0] to vo[0] failed! ret=%d\n", ret);
+    return -1;
   }
 
-  printf("%s exit!\n", __func__);
-  RK_MPI_SYS_UnBind(&stSrcChn, &stDestChn);
+  stSrcChn.enModId = RK_ID_VI;
+  stSrcChn.s32ChnId = 0;
+  stDestChn.enModId = RK_ID_RGA;
+  stDestChn.s32ChnId = 0;
+  ret = RK_MPI_SYS_UnBind(&stSrcChn, &stDestChn);
+  if (ret) {
+    printf("UnBind vi[0] to rga[0] failed! ret=%d\n", ret);
+    return -1;
+  }
+
+  RK_MPI_VO_DestroyChn(0);
   RK_MPI_VI_DisableChn(0, 0);
-  RK_MPI_ALGO_OD_DestroyChn(0);
+  RK_MPI_RGA_DestroyChn(0);
 
   return 0;
 }
