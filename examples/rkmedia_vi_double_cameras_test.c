e1a904a57052935b5397d961b70d7ee8a47d1bb9
Zhihua Wang <hogan.wang@rock-chips.com>
2020-10-12 10:20:35 +0800

    c api: examples: modify rkmedia_vi_double_cameras_test
    Signed-off-by: Zhihua Wang <hogan.wang@rock-chips.com>
    Change-Id: If20a71d716c5b06ea65baa7ce572f8034ed3e691

--- examples/rkmedia_vi_double_cameras_test.c
+++ examples/rkmedia_vi_double_cameras_test.c
@@ -11,9 +11,19 @@
 #include <time.h>
 #include <unistd.h>
 
-#include "common/sample_common.h"
+#include "common/sample_double_cam_isp.h"
 #include "rkmedia_api.h"
-#include "rkmedia_venc.h"
+#include <rga/RgaApi.h>
+
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static MEDIA_BUFFER ir_mb;
+static MEDIA_BUFFER rgb_mb;
+static int video_width = 1920;
+static int video_height = 1080;
+static int disp_width = 720;
+static int disp_height = 1280;
+static int scale_width = 720;
+static int scale_height = 640;
 
 static bool quit = false;
 static void sigterm_handler(int sig) {
@@ -21,180 +31,252 @@ static void sigterm_handler(int sig) {
   quit = true;
 }
 
-static FILE *g_file0;
-static FILE *g_file1;
-
-void video_packet_cb(MEDIA_BUFFER mb) {
-  const char *nalu_type = "Unknow";
-  switch (RK_MPI_MB_GetFlag(mb)) {
-  case VENC_NALU_IDRSLICE:
-    nalu_type = "IDR Slice";
-    break;
-  case VENC_NALU_PSLICE:
-    nalu_type = "P Slice";
-    break;
-  default:
-    break;
-  }
-  printf("Camera-%d::Get Video Encoded packet(%s):ptr:%p, fd:%d, size:%zu\n",
-         RK_MPI_MB_GetChannelID(mb), nalu_type, RK_MPI_MB_GetPtr(mb),
-         RK_MPI_MB_GetFD(mb), RK_MPI_MB_GetSize(mb));
-
-  FILE *file = RK_MPI_MB_GetChannelID(mb) ? g_file0 : g_file1;
-  if (file)
-    fwrite(RK_MPI_MB_GetPtr(mb), 1, RK_MPI_MB_GetSize(mb), file);
-
-  RK_MPI_MB_ReleaseBuffer(mb);
+static void scale_buffer(void *src_p, int src_w, int src_h, int src_fmt,
+                         void *dst_p, int dst_w, int dst_h, int dst_fmt,
+                         int rotation) {
+  rga_info_t src, dst;
+  memset(&src, 0, sizeof(rga_info_t));
+  src.fd = -1;
+  src.virAddr = src_p;
+  src.mmuFlag = 1;
+  src.rotation = rotation;
+  rga_set_rect(&src.rect, 0, 0, src_w, src_h, src_w, src_h, src_fmt);
+  memset(&dst, 0, sizeof(rga_info_t));
+  dst.fd = -1;
+  dst.virAddr = dst_p;
+  dst.mmuFlag = 1;
+  rga_set_rect(&dst.rect, 0, 0, dst_w, dst_h, dst_w, dst_h, dst_fmt);
+  if (c_RkRgaBlit(&src, &dst, NULL))
+    printf("%s: rga fail\n", __func__);
 }
 
-static int StreamOn(int vi_pipe, const char *video_node, int width, int height,
-                    IMAGE_TYPE_E img_type, int vi_chn, int venc_chn) {
-  int ret = 0;
-
-  printf("===> StreamOn: Start: vi_pip:%d: video_node:%s, wxh:%dx%d, vi_chn:%d, "
-         "venc_chn:%d <===\n",
-         vi_pipe, video_node, width, height, vi_chn, venc_chn);
-
-  VI_CHN_ATTR_S vi_chn_attr;
-  vi_chn_attr.pcVideoNode = video_node;
-  vi_chn_attr.u32BufCnt = 4;
-  vi_chn_attr.u32Width = width;
-  vi_chn_attr.u32Height = height;
-  vi_chn_attr.enPixFmt = img_type;
-  vi_chn_attr.enWorkMode = VI_WORK_MODE_NORMAL;
-  ret = RK_MPI_VI_SetChnAttr(vi_pipe, vi_chn, &vi_chn_attr);
-  ret |= RK_MPI_VI_EnableChn(vi_pipe, vi_chn);
-  if (ret) {
-    printf("ERROR: Create VI[%d]:%s failed!\n", vi_chn, video_node);
-    return -1;
-  }
-
-  VENC_CHN_ATTR_S venc_chn_attr;
-  venc_chn_attr.stVencAttr.enType = RK_CODEC_TYPE_H264;
-  venc_chn_attr.stVencAttr.imageType = IMAGE_TYPE_NV12;
-  venc_chn_attr.stVencAttr.u32PicWidth = width;
-  venc_chn_attr.stVencAttr.u32PicHeight = height;
-  venc_chn_attr.stVencAttr.u32VirWidth = width;
-  venc_chn_attr.stVencAttr.u32VirHeight = height;
-  venc_chn_attr.stVencAttr.u32Profile = 77;
-  venc_chn_attr.stRcAttr.enRcMode = VENC_RC_MODE_H264CBR;
-  venc_chn_attr.stRcAttr.stH264Cbr.u32Gop = 30;
-  venc_chn_attr.stRcAttr.stH264Cbr.u32BitRate = width * height * 30 / 14;
-  venc_chn_attr.stRcAttr.stH264Cbr.fr32DstFrameRateDen = 1;
-  venc_chn_attr.stRcAttr.stH264Cbr.fr32DstFrameRateNum = 30;
-  venc_chn_attr.stRcAttr.stH264Cbr.u32SrcFrameRateDen = 1;
-  venc_chn_attr.stRcAttr.stH264Cbr.u32SrcFrameRateNum = 30;
-  ret = RK_MPI_VENC_CreateChn(venc_chn, &venc_chn_attr);
-  if (ret) {
-    printf("ERROR: Create Venc[%d] failed! ret=%d\n", venc_chn, ret);
-    return -1;
-  }
-
-  MPP_CHN_S stEncChn;
-  stEncChn.enModId = RK_ID_VENC;
-  stEncChn.s32DevId = 0;
-  stEncChn.s32ChnId = venc_chn;
-  ret = RK_MPI_SYS_RegisterOutCb(&stEncChn, video_packet_cb);
-  if (ret) {
-    printf("ERROR: RegisterOutCb for Venc[%d] failed! ret = %d\n", venc_chn,
-           ret);
-    return -1;
-  }
-
-  MPP_CHN_S stSrcChn;
-  stSrcChn.enModId = RK_ID_VI;
-  stSrcChn.s32DevId = 0;
-  stSrcChn.s32ChnId = vi_chn;
-  MPP_CHN_S stDestChn;
-  stDestChn.enModId = RK_ID_VENC;
-  stDestChn.s32DevId = 0;
-  stDestChn.s32ChnId = venc_chn;
-  ret = RK_MPI_SYS_Bind(&stSrcChn, &stDestChn);
-  if (ret) {
-    printf("ERROR: Bind Vi[%d] and Venc[%d] failed! ret = %d\n", vi_chn,
-           venc_chn, ret);
-    return -1;
-  }
-
-  return 0;
+static void compose_buffer(void *src_p, int src_w, int src_h, int src_fmt,
+                           void *dst_p, int dst_w, int dst_h, int dst_fmt,
+                           int x, int y, int w, int h) {
+  rga_info_t src, dst;
+  memset(&src, 0, sizeof(rga_info_t));
+  src.fd = -1;
+  src.virAddr = src_p;
+  src.mmuFlag = 1;
+  rga_set_rect(&src.rect, 0, 0, src_w, src_h, src_w, src_h, src_fmt);
+  memset(&dst, 0, sizeof(rga_info_t));
+  dst.fd = -1;
+  dst.virAddr = dst_p;
+  dst.mmuFlag = 1;
+  rga_set_rect(&dst.rect, x, y, w, h, dst_w, dst_h, dst_fmt);
+  if (c_RkRgaBlit(&src, &dst, NULL))
+    printf("%s: rga fail\n", __func__);
 }
 
-static int StreamOff(int vi_pipe, int vi_chn, int venc_chn) {
-  int ret = 0;
+static void *GetRgbBuffer(void *arg) {
+  MEDIA_BUFFER mb = NULL;
 
-  MPP_CHN_S stSrcChn;
-  stSrcChn.enModId = RK_ID_VI;
-  stSrcChn.s32DevId = 0;
-  stSrcChn.s32ChnId = vi_chn;
-  MPP_CHN_S stDestChn;
-  stDestChn.enModId = RK_ID_VENC;
-  stDestChn.s32DevId = 0;
-  stDestChn.s32ChnId = venc_chn;
-  ret = RK_MPI_SYS_UnBind(&stSrcChn, &stDestChn);
-  if (ret) {
-    printf("ERROR: UnBind Vi[%d] and Venc[%d] failed! ret = %d\n", vi_chn,
-           venc_chn, ret);
-    return -1;
-  }
-  ret = RK_MPI_VI_DisableChn(vi_pipe, vi_chn);
-  if (ret) {
-    printf("ERROR: Destroy Vi[%d] failed! ret = %d\n", vi_chn, ret);
-    return -1;
-  }
-  ret = RK_MPI_VENC_DestroyChn(venc_chn);
-  if (ret) {
-    printf("ERROR: Destroy Venc[%d] failed! ret = %d\n", venc_chn, ret);
-    return -1;
+  (void)arg;
+  while (!quit) {
+    mb = RK_MPI_SYS_GetMediaBuffer(RK_ID_RGA, 0, -1);
+    if (!mb) {
+      printf("RK_MPI_SYS_GetMediaBuffer get null buffer!\n");
+      break;
+    }
+
+    scale_buffer(RK_MPI_MB_GetPtr(mb), disp_width, disp_height,
+                 RK_FORMAT_RGB_888, RK_MPI_MB_GetPtr(rgb_mb), scale_width,
+                 scale_height, RK_FORMAT_RGB_888, 0);
+    compose_buffer(RK_MPI_MB_GetPtr(rgb_mb), scale_width, scale_height,
+                   RK_FORMAT_RGB_888, RK_MPI_MB_GetPtr(mb), disp_width,
+                   disp_height, RK_FORMAT_RGB_888, 0, 0, scale_width,
+                   scale_height);
+    pthread_mutex_lock(&mutex);
+    compose_buffer(RK_MPI_MB_GetPtr(ir_mb), scale_width, scale_height,
+                   RK_FORMAT_RGB_888, RK_MPI_MB_GetPtr(mb), disp_width,
+                   disp_height, RK_FORMAT_RGB_888, 0, scale_height, scale_width,
+                   scale_height);
+    pthread_mutex_unlock(&mutex);
+
+    RK_MPI_SYS_SendMediaBuffer(RK_ID_VO, 0, mb);
+
+    RK_MPI_MB_ReleaseBuffer(mb);
   }
 
-  return 0;
+  return NULL;
 }
 
-static void print_usage(char *name) {
-  printf("#Usage: \n");
-  printf("  %s VideoNode0 VideoNode1\n", name);
-  printf("  VideoNode0: 1920x1080, video node for camera0, such as:\"/dev/video0\"\n");
-  printf("  VideoNode1: 1280x720, video node for camera1, such as:\"/dev/video1\"\n");
+static void *GetIrBuffer(void *arg) {
+  MEDIA_BUFFER mb = NULL;
+
+  (void)arg;
+  while (!quit) {
+    mb = RK_MPI_SYS_GetMediaBuffer(RK_ID_VI, 1, -1);
+    if (!mb) {
+      printf("RK_MPI_SYS_GetMediaBuffer get null buffer!\n");
+      break;
+    }
+
+    pthread_mutex_lock(&mutex);
+    scale_buffer(RK_MPI_MB_GetPtr(mb), video_width, video_height,
+                 RK_FORMAT_YCbCr_420_SP, RK_MPI_MB_GetPtr(ir_mb), scale_width,
+                 scale_height, RK_FORMAT_RGB_888, HAL_TRANSFORM_ROT_270);
+    pthread_mutex_unlock(&mutex);
+
+    RK_MPI_MB_ReleaseBuffer(mb);
+  }
+
+  return NULL;
 }
 
 int main(int argc, char *argv[]) {
   int ret = 0;
+  pthread_t trgb, tir;
+  MB_IMAGE_INFO_S disp_info = {scale_width, scale_height, scale_width,
+                               scale_height, IMAGE_TYPE_RGB888};
 
-  if (argc != 3) {
-    print_usage(argv[0]);
+  (void)argc;
+  (void)argv;
+
+  rk_aiq_sys_ctx_t *ctx0 = aiq_double_cam_init(0, RK_AIQ_WORKING_MODE_NORMAL);
+  if (!ctx0)
+    return -1;
+  rk_aiq_sys_ctx_t *ctx1 = aiq_double_cam_init(1, RK_AIQ_WORKING_MODE_NORMAL);
+  if (!ctx1)
+    return -1;
+
+  rgb_mb = RK_MPI_MB_CreateImageBuffer(&disp_info, RK_TRUE);
+  if (!rgb_mb) {
+    printf("ERROR: no space left!\n");
+    return -1;
+  }
+  ir_mb = RK_MPI_MB_CreateImageBuffer(&disp_info, RK_TRUE);
+  if (!ir_mb) {
+    printf("ERROR: no space left!\n");
     return -1;
   }
 
-  printf("#Camera0:%s, wxh:1080p\n", argv[1]);
-  printf("#Camera1:%s, wxh:720p\n", argv[2]);
-
   RK_MPI_SYS_Init();
+  VI_CHN_ATTR_S vi_chn_attr;
+  memset(&vi_chn_attr, 0, sizeof(vi_chn_attr));
+  vi_chn_attr.pcVideoNode = "rkispp_scale1";
+  vi_chn_attr.u32BufCnt = 4;
+  vi_chn_attr.u32Width = video_width;
+  vi_chn_attr.u32Height = video_height;
+  vi_chn_attr.enPixFmt = IMAGE_TYPE_NV12;
+  vi_chn_attr.enWorkMode = VI_WORK_MODE_NORMAL;
+  ret = RK_MPI_VI_SetChnAttr(0, 0, &vi_chn_attr);
+  ret |= RK_MPI_VI_EnableChn(0, 0);
+  if (ret) {
+    printf("Create vi[0] failed! ret=%d\n", ret);
+    return -1;
+  }
 
-  g_file0 = fopen("/userdata/camera0.h264", "w");
-  g_file1 = fopen("/userdata/camera1.h264", "w");
+  memset(&vi_chn_attr, 0, sizeof(vi_chn_attr));
+  vi_chn_attr.pcVideoNode = "rkispp_scale1";
+  vi_chn_attr.u32BufCnt = 4;
+  vi_chn_attr.u32Width = video_width;
+  vi_chn_attr.u32Height = video_height;
+  vi_chn_attr.enPixFmt = IMAGE_TYPE_NV12;
+  vi_chn_attr.enWorkMode = VI_WORK_MODE_NORMAL;
+  ret = RK_MPI_VI_SetChnAttr(1, 1, &vi_chn_attr);
+  ret |= RK_MPI_VI_EnableChn(1, 1);
+  if (ret) {
+    printf("Create vi[1] failed! ret=%d\n", ret);
+    return -1;
+  }
 
-  ret = StreamOn(0, argv[1], 1920, 1080, IMAGE_TYPE_NV12, 0, 0);
-  if (ret)
-    exit(0);
+  RGA_ATTR_S stRgaAttr;
+  memset(&stRgaAttr, 0, sizeof(stRgaAttr));
+  stRgaAttr.bEnBufPool = RK_TRUE;
+  stRgaAttr.u16BufPoolCnt = 12;
+  stRgaAttr.u16Rotaion = 90;
+  stRgaAttr.stImgIn.u32X = 0;
+  stRgaAttr.stImgIn.u32Y = 0;
+  stRgaAttr.stImgIn.imgType = IMAGE_TYPE_NV12;
+  stRgaAttr.stImgIn.u32Width = video_width;
+  stRgaAttr.stImgIn.u32Height = video_height;
+  stRgaAttr.stImgIn.u32HorStride = video_width;
+  stRgaAttr.stImgIn.u32VirStride = video_height;
+  stRgaAttr.stImgOut.u32X = 0;
+  stRgaAttr.stImgOut.u32Y = 0;
+  stRgaAttr.stImgOut.imgType = IMAGE_TYPE_RGB888;
+  stRgaAttr.stImgOut.u32Width = disp_width;
+  stRgaAttr.stImgOut.u32Height = disp_height;
+  stRgaAttr.stImgOut.u32HorStride = disp_width;
+  stRgaAttr.stImgOut.u32VirStride = disp_height;
+  ret = RK_MPI_RGA_CreateChn(0, &stRgaAttr);
+  if (ret) {
+    printf("Create rga[0] falied! ret=%d\n", ret);
+    return -1;
+  }
 
-  ret = StreamOn(1, argv[2], 1280, 720, IMAGE_TYPE_NV12, 1, 1);
-  if (ret)
-    exit(0);
+  VO_CHN_ATTR_S stVoAttr = {0};
+  stVoAttr.emPlaneType = VO_PLANE_OVERLAY;
+  stVoAttr.enImgType = IMAGE_TYPE_RGB888;
+  stVoAttr.u16Fps = 60;
+  stVoAttr.u16Zpos = 0;
+  stVoAttr.u32Width = disp_width;
+  stVoAttr.u32Height = disp_height;
+  stVoAttr.u32HorStride = disp_width;
+  stVoAttr.u32VerStride = disp_height;
+  ret = RK_MPI_VO_CreateChn(0, &stVoAttr);
+  if (ret) {
+    printf("Create vo[0] failed! ret=%d\n", ret);
+    return -1;
+  }
+
+  MPP_CHN_S stSrcChn = {0};
+  MPP_CHN_S stDestChn = {0};
+
+  stSrcChn.enModId = RK_ID_VI;
+  stSrcChn.s32ChnId = 0;
+  stDestChn.enModId = RK_ID_RGA;
+  stDestChn.s32ChnId = 0;
+  ret = RK_MPI_SYS_Bind(&stSrcChn, &stDestChn);
+  if (ret) {
+    printf("Bind vi[0] to rga[0] failed! ret=%d\n", ret);
+    return -1;
+  }
+
+  if (pthread_create(&trgb, NULL, GetRgbBuffer, NULL)) {
+    printf("create GetRgbBuffer thread failed!\n");
+    return -1;
+  }
+  if (pthread_create(&tir, NULL, GetIrBuffer, NULL)) {
+    printf("create GetIrBuffer thread failed!\n");
+    return -1;
+  }
+  ret = RK_MPI_VI_StartStream(0, 1);
+  if (ret) {
+    printf("Start Vi[1] failed! ret=%d\n", ret);
+    return -1;
+  }
 
   printf("%s initial finish\n", __func__);
   signal(SIGINT, sigterm_handler);
-
   while (!quit) {
     usleep(100);
   }
 
-  StreamOff(0, 0, 0);
-  StreamOff(1, 1, 1);
+  printf("%s exit!\n", __func__);
+  pthread_join(trgb, NULL);
+  pthread_join(tir, NULL);
+  stSrcChn.enModId = RK_ID_VI;
+  stSrcChn.s32ChnId = 0;
+  stDestChn.enModId = RK_ID_RGA;
+  stDestChn.s32ChnId = 0;
+  ret = RK_MPI_SYS_UnBind(&stSrcChn, &stDestChn);
+  if (ret) {
+    printf("UnBind vi[0] to rga[0] failed! ret=%d\n", ret);
+    return -1;
+  }
 
-  if (g_file0)
-    fclose(g_file0);
-  if (g_file1)
-    fclose(g_file1);
+  RK_MPI_VO_DestroyChn(0);
+  RK_MPI_VI_DisableChn(0, 0);
+  RK_MPI_VI_DisableChn(0, 1);
+  RK_MPI_RGA_DestroyChn(0);
+  RK_MPI_RGA_DestroyChn(1);
+  RK_MPI_MB_ReleaseBuffer(rgb_mb);
+  RK_MPI_MB_ReleaseBuffer(ir_mb);
+
+  aiq_double_cam_exit(ctx0);
+  aiq_double_cam_exit(ctx1);
 
   return 0;
 }
